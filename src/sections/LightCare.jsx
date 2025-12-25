aa5693decd0e2325322c6bcf38d679692b60a2a0
hyc <craftyun83@gmail.com>
2025-12-25 19:18:09 +0800

    added logo as requestued

--- src/sections/LightCare.jsx
+++ src/sections/LightCare.jsx
@@ -1,29 +1,44 @@
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Text, Image, Box, Flex } from "@chakra-ui/react";
 import useWindowDimensions from '../hooks/useWindowDimensions.jsx';
-
 
 function LightCare() {
   const { width } = useWindowDimensions();
+  const isDesktop = width > 1200;
+
   return (
     <div className="content" id="mainContent">
       <Grid
-        templateColumns={width > 1200 ? "1fr 2fr" : "1fr"}
+        templateColumns={isDesktop ? "1fr 2fr" : "1fr"}
         gap={"2%"}
-        w={width > 1200 ? "80vw" : "95vw"}
-        px={width > 1200 ? 4 : 16}
-        py={width > 1200 ? 8 : 16}
-        textAlign={width > 1200 ? "left" : "center"}
+        w={isDesktop ? "80vw" : "95vw"}
+        px={isDesktop ? 4 : 16}
+        py={isDesktop ? 8 : 16}
+        textAlign={isDesktop ? "left" : "center"}
         alignItems="start"
       >
-        <GridItem >
-          <Text fontWeight="bold" fontSize={width > 1200 ? "5xl" : "4xl"}>
+        <GridItem>
+          <Text fontWeight="bold" fontSize={isDesktop ? "5xl" : "4xl"}>
             WHAT IS <br /> LIGHT CARE?
           </Text>
-          <div style={{ marginTop: "10px", marginBottom: "20px", backgroundColor: "#ffbe5cff", width: (width > 1200 ? "80%" : "100%"), height: "10px" }} />
+          
+          <Box 
+            mt="10px" 
+            mb="20px" 
+            bg="#ffbe5cff" 
+            w={isDesktop ? "80%" : "100%"} 
+            h="10px" 
+          />
+          <Flex justify="center" w="100%" mb={6}>
+            <Image 
+              src="media/logo.png" 
+              alt="Light Care Logo" 
+              maxH="300px" 
+            />
+          </Flex>
         </GridItem>
 
         <GridItem>
-          <Text fontSize={width > 1200 ? "xl" : "l"} lineHeight="taller">
+          <Text fontSize={isDesktop ? "xl" : "lg"} lineHeight="taller">
             Light Care is more than just a mirror, it is an AI-powered wellness mirror that provides quick, contactless insights during a user's normal morning routine such as while brushing their teeth.
             <br /><br />
             Using a camera attached, it will be able to analyse how light reflects off the face to detect patterns related to hydration, fatigue, skin condition, and heart rate trends. Based on data and a trained AI model, the screen on the smart mirror will display a quick overview of your scan, and provide personalized guidance for your day, or lifestyle adjustments.
@@ -34,4 +49,4 @@ function LightCare() {
   )
 }
 
-export default LightCare
+export default LightCare;
